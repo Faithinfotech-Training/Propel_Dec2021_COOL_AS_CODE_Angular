1b67b322ce725fd86b21ac60b68fde19c47be014
TeenaShajan <teenashajan96@gmail.com>
2022-05-12 16:20:22 +0530

    Added entire files

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,31 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { StaffComponent } from './staff/staff.component';
+import { LoginComponent } from './login/login.component';
+import { StaffDashboardComponent } from './staff-dashboard/staff-dashboard.component';
+import { LabtestComponent } from './labtest/labtest.component';
+import { MedicineComponent } from './medicine/medicine.component';
+import { PatientComponent } from './patient/patient.component';
+import { AppointmentlistComponent } from './appointmentlist/appointmentlist.component';
+import { ViewlabtestComponent } from './viewlabtest/viewlabtest.component';
+import { ViewmedicineComponent } from './viewmedicine/viewmedicine.component';
+import { AuthGuard } from './service/auth.guard';
 
-const routes: Routes = [];
+const routes: Routes = [
+  
+  { path: '', component: LoginComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'applist', component: AppointmentlistComponent , canActivate: [AuthGuard],data:{role:'3'}},
+  { path: 'staff', component: StaffComponent , canActivate: [AuthGuard],data:{role:'1'}},
+  { path: 'staffdash', component: StaffDashboardComponent , canActivate: [AuthGuard],data:{role:'1'}},
+  { path: 'labtest', component: LabtestComponent , canActivate: [AuthGuard],data:{role:'1'}},
+  { path: 'medicine', component: MedicineComponent, canActivate: [AuthGuard],data:{role:'1'} },
+  { path: 'patient', component: PatientComponent , canActivate: [AuthGuard],data:{role:'2'}},
+  { path: 'labs', component: ViewlabtestComponent , canActivate: [AuthGuard],data:{role:'4'}},
+  { path: 'meds', component: ViewmedicineComponent , canActivate: [AuthGuard],data:{role:'5'}},
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'PropelDec2021COOLASCODEAngular'`, () => {
+  it(`should have as title 'Cool-As-Code'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('PropelDec2021COOLASCODEAngular');
+    expect(app.title).toEqual('Cool-As-Code');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('PropelDec2021COOLASCODEAngular app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('Cool-As-Code app is running!');
   });
 });
